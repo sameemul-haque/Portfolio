8e43362d26fc798e38d82ec21de598b8dfa198f7
sameemul-haque <samhaqk@gmail.com>
2023-10-08 18:52:14 +0530

    skills page fixed fixed height issue fixed icon color issue

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -28,7 +28,7 @@ export default function SkillsPage({ skills: rawSkills }: SkillsProps): JSX.Elem
 
   return (
     <Layout.Default seo={{ title: 'Skills' }}>
-      <div className="flex flex-grow  pt-16 pb-12">
+      <div className="flex flex-grow min-h-screen pt-16 pb-12">
         <div className="flex-grow grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {skills.map((event, index) => (
           <Animate
@@ -44,7 +44,7 @@ export default function SkillsPage({ skills: rawSkills }: SkillsProps): JSX.Elem
                 rel="noopener noreferrer"
                 className="relative flex items-center space-x-3 bg-gray-50 bg-opacity-75 dark:bg-gray-900 dark:bg-opacity-75 backdrop-filter backdrop-blur-sm px-2 py-3 border-2 border-gray-400 dark:border-gray-600 rounded-lg"
               >
-                <Icon icon={event.icon} className="w-6 h-6 text-primary-500" />
+                <Icon icon={event.icon} className="w-6 h-6 text-gray-500 dark:text-white" />
                 <span className="text-gray-500 dark:text-white text-lg tracking-tight font-bold">
                   {event.title}
                 </span>
