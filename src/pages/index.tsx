a2cf802d3a6bab7df1224067b93bb74241843712
sameemul-haque <samhaqk@gmail.com>
2024-06-09 19:49:49 +0530

    chore: bing and yandex search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic';
 import { Icon } from '@iconify/react';
+import Head from 'next/head'
 
 import { Animate, Button, Pill } from '~/components';
 import { EventType, NavigationItemType } from '~/types';
@@ -59,61 +60,64 @@ export default function HomePage(): JSX.Element {
 	const description = `I am a B.Tech student & software engineer.`;
 
 	return (
-		<Layout.Default>
-			{isBirthday && <Event event={EventType.BIRTHDAY} />}
-			<div className="min-h-screen flex items-center justify-center py-12">
-				<div className="max-w-md sm:max-w-lg md:sm:max-w-2xl lg:sm:max-w-3xl w-full space-y-8 text-center">
-					<Animate
-						as="h1"
-						animation={{
-							opacity: [0, 1],
-							scale: [0.75, 1],
-						}}
-						className="text-gray-800 dark:text-white text-2xl sm:text-3xl md:text-5xl lg:text-7xl tracking-tight font-extrabold  backdrop-filter backdrop-blur-sm saturate-200 ">
-						Hey <span className="inline-block origin-70 animate-wave">👋</span> I&apos;m
-						<br />
-						Sameemul Haque
-						<br />a <Pill.Standard className="mt-4">developer</Pill.Standard>
-					</Animate>
+		<>
+			<Head><meta name="yandex-verification" content="e3e66437a30d98e6" /></Head>
+			<Layout.Default>
+				{isBirthday && <Event event={EventType.BIRTHDAY} />}
+				<div className="min-h-screen flex items-center justify-center py-12">
+					<div className="max-w-md sm:max-w-lg md:sm:max-w-2xl lg:sm:max-w-3xl w-full space-y-8 text-center">
+						<Animate
+							as="h1"
+							animation={{
+								opacity: [0, 1],
+								scale: [0.75, 1],
+							}}
+							className="text-gray-800 dark:text-white text-2xl sm:text-3xl md:text-5xl lg:text-7xl tracking-tight font-extrabold  backdrop-filter backdrop-blur-sm saturate-200 ">
+							Hey <span className="inline-block origin-70 animate-wave">👋</span> I&apos;m
+							<br />
+							Sameemul Haque
+							<br />a <Pill.Standard className="mt-4">developer</Pill.Standard>
+						</Animate>
 
-					<Animate
-						as="p"
-						animation={{
-							opacity: [0, 1],
-							scale: [0.75, 1],
-						}}
-						className="max-w-xs mt-4 md:mt-8 mx-auto text-base text-gray-400 sm:text-lg md:text-xl md:max-w-3xl backdrop-filter backdrop-blur-sm saturate-200"
-						transition={{
-							delay: 0.5,
-						}}>
-						{description}
-					</Animate>
+						<Animate
+							as="p"
+							animation={{
+								opacity: [0, 1],
+								scale: [0.75, 1],
+							}}
+							className="max-w-xs mt-4 md:mt-8 mx-auto text-base text-gray-400 sm:text-lg md:text-xl md:max-w-3xl backdrop-filter backdrop-blur-sm saturate-200"
+							transition={{
+								delay: 0.5,
+							}}>
+							{description}
+						</Animate>
 
-					<div className="flex flex-col sm:flex-row items-center justify-center sm:space-x-3 space-y-4 sm:space-y-0 w-full mt-8 sm:mt-4">
-						{ACTIONS.map((action, index) => {
-							if (action.type !== NavigationItemType.LINK) return null;
+						<div className="flex flex-col sm:flex-row items-center justify-center sm:space-x-3 space-y-4 sm:space-y-0 w-full mt-8 sm:mt-4">
+							{ACTIONS.map((action, index) => {
+								if (action.type !== NavigationItemType.LINK) return null;
 
-							return (
-								<Animate
-									animation={{
-										y: [50, 0],
-										opacity: [0, 1],
-									}}
-									className="w-full sm:w-auto"
-									key={index}
-									transition={{
-										delay: 0.1 * (index + 2) + 0.5,
-									}}>
-									<Button.Outline href={action.href}>
-										{action.icon}
-										<span>{action.text}</span>
-									</Button.Outline>
-								</Animate>
-							);
-						})}
+								return (
+									<Animate
+										animation={{
+											y: [50, 0],
+											opacity: [0, 1],
+										}}
+										className="w-full sm:w-auto"
+										key={index}
+										transition={{
+											delay: 0.1 * (index + 2) + 0.5,
+										}}>
+										<Button.Outline href={action.href}>
+											{action.icon}
+											<span>{action.text}</span>
+										</Button.Outline>
+									</Animate>
+								);
+							})}
+						</div>
 					</div>
 				</div>
-			</div>
-		</Layout.Default>
+			</Layout.Default>
+		</>
 	);
 }
