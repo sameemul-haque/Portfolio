498e8d8dd88eade64e88951a97799e5c28f43859
sameemul-haque <samhaqk@gmail.com>
2026-09-25 14:18:24 +0530

    Fix sitemap: drop nonexistent server-sitemap, exclude error and blog pages

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,13 +6,11 @@ const protocol = isProduction ? 'https' : 'http';
  * @type {import('next-sitemap').IConfig}
  */
 module.exports = {
-	exclude: ['/server-sitemap.xml'],
+	// Error pages and blog routes (redirected to sameem.dev) should not be indexed
+	exclude: ['/404', '/500', '/error', '/blog', '/blog/*'],
 	generateRobotsTxt: true,
 	robotsTxtOptions: {
-		additionalSitemaps: [
-			`${protocol}://${domain}/sitemap.xml`,
-			`${protocol}://${domain}/server-sitemap.xml`,
-		],
+		policies: [{ userAgent: '*', allow: '/', disallow: ['/error'] }],
 	},
 	siteUrl: `${protocol}://${domain}`,
 };
